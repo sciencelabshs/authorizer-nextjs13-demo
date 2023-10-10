ef604ded4a3e93d1e86eaa88ac65c9a4dd9ac993
sciencelabshs <vlabshs@gmail.com>
2023-10-10 19:38:54 +0530

    config update

--- config/authorizer-config.js
+++ config/authorizer-config.js
@@ -1,5 +1,5 @@
 export default {
-  authorizerURL: 'https://home-auth.sciencelabshs.com/',
-  redirectURL: 'http://localhost:3000',
+  authorizerURL: 'https://home-auth.sciencelabshs.com',
+  redirectURL: 'https://authorizer-nextjs13-demo.vercel.app',
   clientID: '36c72ba4-b7cc-4859-8353-90f2c8e3769c',
 };
